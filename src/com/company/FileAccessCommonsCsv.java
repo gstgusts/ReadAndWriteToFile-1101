23892fb6c821de5e019a82a1a75c296bb2df9195
Gusts Linkevics <gusts@gst.lv>
2021-02-02 20:45:27 +0200

    Added libraries

---
+++ src/com/company/FileAccessCommonsCsv.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileAccessCommonsCsv {
+    public void write(List<Product> products) {
+
+    }
+
+    public List<Product> read() {
+        return new ArrayList<>();
+    }
+}
